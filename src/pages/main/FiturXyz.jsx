f2aba3528b3b6ae365bbffbe77f6ba1c353c629c
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-05-25 15:03:37 +0700

    xyz

--- src/pages/main/FiturXyz.jsx
+++ src/pages/main/FiturXyz.jsx
@@ -4,7 +4,7 @@ export default function FiturXyz() {
   return (
     <div>
       <h1>Fitur Xyz</h1>
-      <p>Ini adalah halaman untuk menampilkan fitur Xyz.</p>
+      <p>Ini adalah halaman untuk menampilkan fitur Xyz..</p>
     </div>
   );
 }
